96e144ef3673de7b9e27ab586d908bb346227964
Saif Ali <saifali7646@gmail.com>
2021-09-01 16:41:56 -0400

    Building the Daylist and Updating our App

---
+++ src/components/DayListItem.js
@@ -0,0 +1,28 @@
+import React from "react";
+import classNames from 'classnames/bind';
+import "components/DayListItem.scss";
+
+
+
+export default function DayListItem(props) {
+  
+  const formatSpots = () => (
+  
+    props.spots ? `${props.spots} spot${props.spots > 1 ? 's' : ''} remaining` : "no spots remaining"
+    
+  )
+
+  const dayClass = classNames("day-list__item", {
+    "day-list__item--selected": props.selected,
+    "day-list__item--full": props.spots
+  });
+  
+  return (
+    <li className= {dayClass} onClick={() => props.setDay(props.name)}>
+      <h2 className={dayClass}>{props.name}</h2>
+      <h3 className={dayClass}>{formatSpots()}</h3>
+    </li>
+  );
+}
+
+
