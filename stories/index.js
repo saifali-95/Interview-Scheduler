24231264f154a69c11a665c0fa98c131a2a5da6d
Saif Ali <saifali7646@gmail.com>
2021-09-02 17:46:43 -0400

    Form Edit and Create Stories

--- stories/index.js
+++ stories/index.js
@@ -13,6 +13,7 @@ import  Show  from "../src/components/Appointment/Show";
 import  Confirm  from "../src/components/Appointment/Confirm";
 import  Status  from "../src/components/Appointment/Status";
 import  Error  from "../src/components/Appointment/Error";
+import  Form  from "../src/components/Appointment/Form";
 
 import "index.scss";
 
@@ -162,3 +163,22 @@ storiesOf("Appointment", module)
   )
   .add("Status", ()=> (<Status message = "Deleting"/>))
   .add("Error", ()=> (<Error message = "Could not delete appointment." onClose = {action("onClose")}/>))
+  
+  .add("Edit", ()=> (
+    <Form name = "Lydia Miller-Jones" 
+    interviewers = {interviewers}
+    interviewer = {3}
+    onSave = {action("onSave")} 
+    onCancel = {action("onCancel")} 
+    />
+    )
+  )
+
+  .add("Create", ()=> (
+    <Form  
+    interviewers = {interviewers}
+    onSave = {action("onSave")} 
+    onCancel = {action("onCancel")} 
+    />
+    )
+  )
