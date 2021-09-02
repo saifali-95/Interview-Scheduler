c311c8ed789ca40d7a3098447332622b69125198
Saif Ali <saifali7646@gmail.com>
2021-09-02 15:25:23 -0400

    Error Component and Error Stories

--- stories/index.js
+++ stories/index.js
@@ -12,6 +12,7 @@ import  Empty  from "../src/components/Appointment/Empty";
 import  Show  from "../src/components/Appointment/Show";
 import  Confirm  from "../src/components/Appointment/Confirm";
 import  Status  from "../src/components/Appointment/Status";
+import  Error  from "../src/components/Appointment/Error";
 
 import "index.scss";
 
@@ -160,3 +161,4 @@ storiesOf("Appointment", module)
     )
   )
   .add("Status", ()=> (<Status message = "Deleting"/>))
+  .add("Error", ()=> (<Error message = "Could not delete appointment." onClose = {action("onClose")}/>))
