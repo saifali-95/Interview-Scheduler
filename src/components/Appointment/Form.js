3ad78c9f965590dddca19ff8d57cda50e6977e40
Saif Ali <saifali7646@gmail.com>
2021-09-02 20:16:45 -0400

    add cancel and reset function to Form component

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import  InterviewerList  from "../InterviewerList";
 import Button from "../Button";
 
 
 export default function Form(props) {  
   
+  const [name, setName] = useState(props.name || "");
+  const [interviewer, setInterviewer] = useState(props.interviewer || null);
+
+  const reset = function () {
+    setInterviewer("null")
+    setName("")  
+  }
+
+  const cancel = function () {
+    reset()
+    props.onCancel()
+  }
+
   return (
     <main className="appointment__card appointment__card--create">
     <section className="appointment__card-left">
@@ -14,16 +27,19 @@ export default function Form(props) {
           name="name"
           type="text"
           placeholder= {props.name ? `${props.name}` : "Enter Student Name"}
+          onChange={(event) => setName(event.target.value)}
           /*
             This must be a controlled component
           */
         />
       </form>
-      <InterviewerList interviewers={props.interviewers} interviewer={props.interviewer} onChange={props.setInterviewer} />
+      <InterviewerList interviewers={props.interviewers} 
+                      interviewer={interviewer} 
+                      onChange={setInterviewer} />
     </section>
     <section className="appointment__card-right">
       <section className="appointment__actions">
-        <Button danger onClick = {props.onCancel}>Cancel</Button>
+        <Button danger onClick = {() => cancel()}>Cancel</Button>
         <Button confirm onClick = {props.onSave}>Save</Button>
       </section>
     </section>
--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -7,14 +7,14 @@ import  InterviewerListItem  from "./InterviewerListItem";
 export default function InterviewerList(props) {
 
   const { interviewers } = props
-  const person =  interviewers.map((person) => {
+  const interviewers__list =  interviewers.map((interviewer) => {
     return (
       <InterviewerListItem 
-        key={person.id} 
-        name= {person.name} 
-        avatar = {person.avatar} 
-        selected={person.id === props.interviewer} 
-        setInterviewer={(event) => props.onChange(person.id)}
+        key={interviewer.id} 
+        name= {interviewer.name} 
+        avatar = {interviewer.avatar} 
+        selected={interviewer.id === props.interviewer} 
+        setInterviewer={(event) => props.onChange(interviewer.id)}
       />
     )
   })
@@ -24,7 +24,7 @@ export default function InterviewerList(props) {
     
   <section className="interviewers">
   <h4 className="interviewers__header text--light">Interviewer</h4>
-  <ul className="interviewers__list"> {person}  </ul>
+  <ul className="interviewers__list"> {interviewers__list}  </ul>
   
   </section>
   
